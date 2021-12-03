17bcfb1330f79427f8c24d6de912380cf73bd3c8
RamanSlinka <romuch1986@gmail.com>
2021-12-03 22:46:07 +0100

    added styles for SalePage (+icons)

--- src/pages/SalePage.tsx
+++ src/pages/SalePage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import app_mobile from './../assets/img/app_mobile.png'
+import {BsArrowRightCircle} from "react-icons/all";
 
 const SalePage = () => {
     return (
@@ -17,20 +18,20 @@ const SalePage = () => {
                         What should be done:
                     </h4>
                     <ol>
-                        <li>download the "PIZZA SHOP" app from the App Store or Google Play</li>
-                        <li>register</li>
-                        <li>come to the PIZZA SHOP Rondo ONZ pizzeria</li>
-                        <li>show the cashier your active profile in the app</li>
-                        <li>enter the promotional code COOL_PIZZA and the phone number for which the profile was
+                        <li> <BsArrowRightCircle className="sale__icon"/>&ensp;download the "PIZZA SHOP" app from the App Store or Google Play</li>
+                        <li><BsArrowRightCircle className="sale__icon"/>&ensp;register</li>
+                        <li><BsArrowRightCircle className="sale__icon"/>&ensp;come to the PIZZA SHOP Rondo ONZ pizzeria</li>
+                        <li><BsArrowRightCircle className="sale__icon"/>&ensp;show the cashier your active profile in the app</li>
+                        <li><BsArrowRightCircle className="sale__icon"/>&ensp;enter the promotional code COOL_PIZZA and the phone number for which the profile was
                             registered
                         </li>
-                        <li>Receive a tasty hot piece of PIZZA SHOP for free</li>
+                        <li><BsArrowRightCircle className="sale__icon"/>&ensp;Receive a tasty hot piece of PIZZA SHOP for free</li>
                     </ol>
                 <h4>Conditions of the action:</h4>
                     <ol>
-                        <li>the action lasts until May 30, 2022</li>
-                        <li>valid at the  Pizza Shop pizzeria ul. Bukowska, 3 Poznan</li>
-                        <li>1 user can redeem the code 1 time and receive 1 piece of free pizza</li>
+                        <li><BsArrowRightCircle className="sale__icon"/>&ensp;the action lasts until May 30, 2022</li>
+                        <li><BsArrowRightCircle className="sale__icon"/>&ensp;valid at the  Pizza Shop pizzeria ul. Bukowska, 3 Poznan</li>
+                        <li><BsArrowRightCircle className="sale__icon"/>&ensp;1 user can redeem the code 1 time and receive 1 piece of free pizza</li>
                     </ol>
                 </div>
 
