654f6300cff309c2ca8e6e5747554aca3acf8b84
RamanSlinka <romuch1986@gmail.com>
2022-06-29 23:06:49 +0200

    changed RoutesPage for future(adding sign in + sign up )

--- src/components/RoutesPage.tsx
+++ src/components/RoutesPage.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import AboutUs from "../pages/AboutUs";
 import Home from "../pages/Home";
-import Cart from "../pages/Cart";
+import AboutUs from "../pages/AboutUs";
+import AboutApp from "./AboutApp";
 import Contacts from "../pages/Contacts";
+import Cart from "../pages/Cart";
 import SalePage from "../pages/SalePage";
 import WelcomePage from "../pages/WelcomePage";
-import AboutApp from "./AboutApp";
 import FeedbackPage from "./FeedbackPage";
+import Registration from "../pages/Registration";
+import Login from "../pages/Login";
+
 
 export const PATH = {
     HOME: '/',
@@ -17,22 +20,46 @@ export const PATH = {
     CONTACTS: '/contacts',
     SALE: '/sale',
     WELCOME: '/pizza-shop-v.2',
-    FEEDBACK: '/feedback'
+    FEEDBACK: '/feedback',
+    ADMIN: '/admin',
+    BASKET: '/basket',
+    LOGIN: '/login',
+    REGISTRATION: '/registration',
 }
 
 
 const RoutesPage = () => {
+    const isAuth = false
+
+
     return (
-        <Routes>
-            <Route path={PATH.HOME} element={<Home/>}/>
-            <Route path={PATH.ABOUT_US} element={<AboutUs/>}/>
-            <Route path={PATH.ABOUT_APP} element={<AboutApp/>}/>
-            <Route path={PATH.CONTACTS} element={<Contacts/>}/>
-            <Route path={PATH.CART} element={<Cart/>}/>
-            <Route path={PATH.SALE} element={<SalePage/>}/>
-            <Route path={PATH.WELCOME} element={<WelcomePage/>}/>
-            <Route path={PATH.FEEDBACK} element={<FeedbackPage/>}/>
-        </Routes>
+        <>
+            {!isAuth ? <Routes>
+                    <Route path={PATH.HOME} element={<Home/>}/>
+                    <Route path={PATH.ABOUT_US} element={<AboutUs/>}/>
+                    <Route path={PATH.ABOUT_APP} element={<AboutApp/>}/>
+                    <Route path={PATH.CONTACTS} element={<Contacts/>}/>
+                    {/*<Route path={PATH.CART} element={<Cart/>}/>*/}
+                    <Route path={PATH.SALE} element={<SalePage/>}/>
+                    <Route path={PATH.WELCOME} element={<WelcomePage/>}/>
+                    <Route path={PATH.FEEDBACK} element={<FeedbackPage/>}/>
+                    <Route path={PATH.REGISTRATION} element={<Registration/>}/>
+                    <Route path={PATH.LOGIN} element={<Login/>}/>
+                </Routes>
+                :
+                <Routes>
+                    <Route path={PATH.HOME} element={<Home/>}/>
+                    <Route path={PATH.ABOUT_US} element={<AboutUs/>}/>
+                    <Route path={PATH.ABOUT_APP} element={<AboutApp/>}/>
+                    <Route path={PATH.CONTACTS} element={<Contacts/>}/>
+                    <Route path={PATH.CART} element={<Cart/>}/>
+                    <Route path={PATH.SALE} element={<SalePage/>}/>
+                    <Route path={PATH.WELCOME} element={<WelcomePage/>}/>
+                    <Route path={PATH.FEEDBACK} element={<FeedbackPage/>}/>
+                </Routes>
+            }
+
+        </>
     );
 }
 
