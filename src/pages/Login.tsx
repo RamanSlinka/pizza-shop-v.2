584218decedd1c9f2b36f3d566fda0464866a6fc
RamanSlinka <romuch1986@gmail.com>
2022-07-01 19:02:41 +0200

    added  layout for Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 
-const Registration = () => {
+const Login = () => {
     return (
         <div className="registration__main">
             <section className="registration__section">
                 <div className="registration__container">
                     <div className="registration__content">
                         <form action="" className="signup-form">
-                            <h3 className="form-title" >Create account</h3>
-                            <div className="form-group">
-                                <input type="text" className="form-input" placeholder={'Your Name'}/>
-                            </div>
+                            <h5 className="form-title" >Log in</h5>
+
                             <div className="form-group">
                                 <input type="email" className="form-input" placeholder={'Your Email'}/>
                             </div>
@@ -19,12 +17,12 @@ const Registration = () => {
                                 <input type="password" className="form-input" placeholder={'Password'}/>
                             </div>
                             <div className="form-group">
-                                <input type="submit" value="Sign up" className="form-submit"/>
+                                <input type="submit" value="Sign in" className="form-submit"/>
                             </div>
                         </form>
                         <p className="loginhere">
-                            Have already an account ?
-                            <NavLink to={'/login'} className="loginhere-link"> Login here</NavLink>
+                            Don't have an account?
+                            <NavLink to={'/registration'} className="loginhere-link"> Register here!</NavLink>
                         </p>
                     </div>
                 </div>
@@ -33,4 +31,4 @@ const Registration = () => {
     );
 };
 
-export default Registration;
+export default Login;
