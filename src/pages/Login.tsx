8acd27008ff04336049a1323245e91648ce0ddea
RamanSlinka <romuch1986@gmail.com>
2022-07-12 00:06:34 +0200

    added "formik"

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,48 @@
-import React, {ChangeEvent, useState} from 'react';
-import {NavLink} from "react-router-dom";
+import React from 'react';
+import {Navigate, NavLink} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {login} from "../store/actions/auth";
 import Loader from "../components/Loader";
 import {AppRootStateType} from "../store";
+import {useFormik} from "formik";
+import {FormikErrorType} from "./Registration";
+import {UserStateType} from "../store/reducers/user";
+import {PATH} from "../components/RoutesPage";
+
+
 const Login = () => {
 
-    const [email, setEmail] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
+
     const dispatch = useDispatch()
     const appLoader = useSelector<AppRootStateType, boolean>(state => state.appReducer.loader)
+    const user = useSelector<AppRootStateType, UserStateType>(state => state.user)
+    const isAuth = user.isAuth
 
-    const loginClickHandler = () => {
-        dispatch(login(email, password))
-        setEmail('')
-        setPassword('')
-    }
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {}
+            if (!values.email) {
+                errors.email = 'Required'
+            } else if ((!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email))) {
+                errors.email = 'Invalid email address'
+            }
+            if (!values.password) {
+                errors.password = 'Required'
+            } else if (values.password.length < 4) {
+                errors.password = 'Password must be 4 characters or more'
+            }
+            return errors
+        },
+        onSubmit: values => {
+            dispatch(login(values.email, values.password))
+            formik.resetForm();
+        }
+    })
+
 
     if (appLoader) {
         return (
@@ -23,39 +50,53 @@ const Login = () => {
         )
     }
     return (
-        <div className="registration__main">
-            <section className="registration__section">
-                <div className="registration__container">
-                    <div className="registration__content">
-                        <div className="signup-form">
-                            <h5 className="form-title">Log in</h5>
+        <>
+            {!isAuth
+                ?
+                <div className="registration__main">
+                    <section className="registration__section">
+                        <div className="registration__container">
+                            <div className="registration__content">
+                                <form
+                                    onSubmit={formik.handleSubmit}
+                                    className="signup-form">
+                                    <h5 className="form-title">Log in</h5>
 
-                            <div className="form-group">
-                                <input
-                                    value={email}
-                                    onChange={(event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
-                                    type="email" className="form-input" placeholder={'Your Email'}/>
-                            </div>
-                            <div className="form-group">
-                                <input
-                                    value={password}
-                                    onChange={(event: ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
-                                    type="password" className="form-input" placeholder={'Password'}/>
-                            </div>
-                            <div className="form-group">
-                                <input
-                                    onClick={() => loginClickHandler()}
-                                    type="submit" value="Sign in" className="form-submit"/>
+                                    <div className="form-group">
+                                        {formik.touched.email && formik.errors.email &&
+                                            <span className="inputError">{formik.errors.email}</span>}
+                                        <input
+                                            {...formik.getFieldProps('email')}
+                                            type="email" className="form-input" placeholder={'Your Email'}
+
+                                        />
+
+                                    </div>
+                                    <div className="form-group">
+                                        {formik.touched.password && formik.errors.password &&
+                                            <span className="inputError">{formik.errors.password}</span>}
+                                        <input
+                                            type="password" className="form-input" placeholder={'Password'}
+                                            {...formik.getFieldProps('password')}
+                                        />
+
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="submit" value="Sign in" className="form-submit"/>
+                                    </div>
+                                </form>
+                                <p className="loginhere">
+                                    Don't have an account?
+                                    <NavLink to={PATH.REGISTRATION} className="loginhere-link"> Register here!</NavLink>
+                                </p>
                             </div>
                         </div>
-                        <p className="loginhere">
-                            Don't have an account?
-                            <NavLink to={'/registration'} className="loginhere-link"> Register here!</NavLink>
-                        </p>
-                    </div>
+                    </section>
                 </div>
-            </section>
-        </div>
+                : <Navigate to={PATH.HOME} replace/>
+            }
+
+        </>
     );
 };
 
