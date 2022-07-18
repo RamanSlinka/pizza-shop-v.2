cf487f48223ec2aa8a9b8d1a61c7bc89baa90f25
RamanSlinka <romuch1986@gmail.com>
2022-07-18 22:51:23 +0200

    added modals pack

---
+++ src/components/modals/CreateImage.tsx
@@ -0,0 +1,35 @@
+import React, {FC} from 'react';
+import {Button, Form, Modal} from "react-bootstrap";
+
+const CreateImage:FC<any> = ({show, onHide}) => {
+    return (
+        <div>
+            <Modal
+                show={show}
+                onHide={onHide}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Modal heading
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Control className="Control"
+                              placeholder={'Enter url image of pizza'}
+                        />
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant={"outline-success"} onClick={onHide}>Add</Button>
+                    <Button variant={"outline-danger"} onClick={onHide}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+};;
+
+export default CreateImage;
