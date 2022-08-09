839cf2287733ad5fb9fceb0459ab0737ab95aa1a
RamanSlinka <romuch1986@gmail.com>
2022-08-09 22:54:17 +0200

    working with cart (in progress...) added increment pizza block from item (type error !!!!!!)

--- src/store/actions/cart.ts
+++ src/store/actions/cart.ts
@@ -3,11 +3,15 @@ import {newItemsType} from "../reducers/cart";
 export type AddPizzaToCartActionType = ReturnType<typeof addPizzaToCart>
 export type ClearCartActionType = ReturnType<typeof clearCart>
 export type RemoveCartItemActionType = ReturnType<typeof removeCartItem>
+export type IncrementCartItemActionType = ReturnType<typeof incrementCartItem>
+export type DecrementCartItemActionType = ReturnType<typeof decrementCartItem>
 
 export type PizzasActionType =
     AddPizzaToCartActionType
     | ClearCartActionType
     | RemoveCartItemActionType
+    | IncrementCartItemActionType
+    | DecrementCartItemActionType
 
 export const addPizzaToCart = (pizzaObj: newItemsType) => ({
     type: 'ADD_PIZZA_CART',
@@ -22,4 +26,14 @@ export const removeCartItem = (id: string) => ({
 
 export const clearCart = () => ({
     type: 'CLEAR_CART',
+} as const);
+
+export const incrementCartItem = (id: string) => ({
+    type: 'INCREMENT_CART_ITEM',
+    payload: id,
+} as const);
+
+export const decrementCartItem = (id: string) => ({
+    type: 'DECREMENT_CART_ITEM',
+    payload: id,
 } as const);
--- src/store/reducers/cart.ts
+++ src/store/reducers/cart.ts
@@ -1,5 +1,16 @@
-import {itemType} from "../actions/pizzas";
+
 import {PizzasActionType} from "../actions/cart";
+
+export type itemType = {
+    _id: string
+    imageUrl: string
+    name: string
+    types: Array<number>
+    sizes: Array<number>
+    price: number
+    category: string | null
+    rating: number
+}
 
 export type newItemsType = {
     _id: number
@@ -20,6 +31,23 @@ const initialState: initialStateType = {
 }
 
 
+const getTotalPrice = (arr: Array<any>) => arr.reduce((sum: number, obj: itemType) => obj.price + sum, 0);
+
+const _get = (obj: any, path: any) => {
+    const [firstKey, ...keys] = path.split('.');
+    return keys.reduce((val: any, key: number) => {
+        return val[key];
+    }, obj[firstKey]);
+};
+
+const getTotalSum = (obj: any, path: any) => {
+    return Object.values(obj).reduce((sum, obj) => {
+        const value = _get(obj, path);
+        return sum + value;
+    }, 0);
+};
+
+
 export default function cart(state: initialStateType = initialState, action: PizzasActionType): initialStateType {
     switch (action.type) {
         case  'ADD_PIZZA_CART':
@@ -32,7 +60,7 @@ export default function cart(state: initialStateType = initialState, action: Piz
 
             const allPizzas = [].concat.apply([], Object.values(newItems));
 
-            const totalPrice = allPizzas.reduce((sum: number, obj: any) => {
+            const totalPrice = allPizzas.reduce((sum: number, obj: itemType) => {
                 return obj.price + sum
             }, 0);
 
@@ -47,11 +75,9 @@ export default function cart(state: initialStateType = initialState, action: Piz
             const newItems = {
                 ...state.itemsCart,
             };
-
             const currentTotalPrice = newItems[action.payload].reduce((sum: number, obj: any) => {
                 return obj.price + sum
             }, 0);
-
             const currentTotalCount = newItems[action.payload].length;
 
             delete newItems[action.payload];
@@ -63,6 +89,43 @@ export default function cart(state: initialStateType = initialState, action: Piz
             };
         }
 
+
+        case "INCREMENT_CART_ITEM": {
+
+            const newObjItems = [
+                ...state.itemsCart[action.payload],
+                state.itemsCart[action.payload][0]
+            ];
+            console.log(newObjItems)
+
+
+            const newItems = {
+                ...state.itemsCart,
+
+                [action.payload]: {
+                    itemsCart: newObjItems,
+                    totalPrice: getTotalPrice(newObjItems)
+
+                    //     newObjItems.reduce((sum: number, obj: any) => {
+                    //     return obj.price + sum
+                    // }, 0)
+                }
+            };
+
+            const totalCount = newObjItems.length
+            const totalPrice = getTotalSum(newItems, 'totalPrice')
+
+            debugger
+
+            return {
+                ...state,
+                // @ts-ignore
+                itemsCart: newItems,
+                // @ts-ignore
+                totalPrice,
+                totalCount
+            }
+        }
         case 'CLEAR_CART':
             return {totalPrice: 0, totalCount: 0, itemsCart: {}};
 
