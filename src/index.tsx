551b0cbaeea6faa4ca3893f48b14f95ea694deb8
RamanSlinka <romuch1986@gmail.com>
2022-08-17 22:48:14 +0200

    working with Payments: insert to project Strip.js for payments

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,26 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./store";
+import {Elements} from "@stripe/react-stripe-js";
+import {loadStripe} from "@stripe/stripe-js";
+
+
+
+const stripPromise = loadStripe('pk_test_51LXsh5IugZRUmYNQj6Rp8s2ItpOwUUaGbE5q0TyHzkRfv3FTEHBscrPYgxmyv8swhgeGgWYWEHhefDKDoI5QiqdM00qiSPBiGE');
+// const options = {
+//     clientSecret: '{{CLIENT_SECRET}}',
+// }
+
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
+            <Elements
+                stripe={stripPromise}
+                // options={options}
+            >
             <App/>
+            </Elements>
         </BrowserRouter>
     </Provider>
     ,
